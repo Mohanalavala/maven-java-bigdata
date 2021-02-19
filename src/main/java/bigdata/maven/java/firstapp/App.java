f7111588941418253ebf817d060ba75afd44a408
Mohanalavala <60022212+Mohanalavala@users.noreply.github.com>
2021-02-19 16:09:15 -0600

    added required files

---
+++ src/main/java/bigdata/maven/java/firstapp/App.java
@@ -0,0 +1,14 @@
+package bigdata.maven.java.firstapp;
+
+/**
+ * @Mohan Krishna Alavala
+ *My first maven-java app
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
